3bbe4aa2dec16b496ba04e28bb644337ada55181
IIIoKoJIaDbI <iiiokojiadbi@yandex.ru>
2020-04-10 15:05:26 +0300

    fix: исправить вызов фунцкий

--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,20 @@ import * as popup from './../module/popup.js';
 import * as openInfo from './../module/open-info.js';
 import * as saveInfo from './../module/save-info.js';
 
-const btnEdit = document.querySelector('.btn_type_edit');
-const btnClose = document.querySelector('.btn_type_close');
-const btnSave = document.querySelector('.btn_type_save');
+const btn = document.querySelectorAll('.btn');
 
-btnEdit.addEventListener('click', function () {
-  openInfo.default();
-  popup.default();
+btn.forEach.call(btn, function (element) {
+  element.addEventListener('click', function () {
+    if (this.classList.contains('btn_type_edit')) {
+      openInfo.default();
+      popup.default();
+    } else if (this.classList.contains('btn_type_close')) {
+      popup.default();
+    } else if (this.classList.contains('btn_type_save')) {
+      saveInfo.default();
+      popup.default();
+    } else {
+      console.log('Функционал кнопки не реализован!');
+    }
+  });
 });
-
-btnClose.addEventListener('click', function () {
-  popup.default()
-})
-
-btnSave.addEventListener('click', function () {
-  saveInfo.default();
-  popup.default();
-})
