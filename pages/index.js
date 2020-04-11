97040d44afce14592602b5b93b3c8eed965d1af7
IIIoKoJIaDbI <iiiokojiadbi@yandex.ru>
2020-04-11 17:49:17 +0300

    fix: исправление основных ошибок после кодревью

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,7 @@
   Функция открытия и закрытия popup.
   При закрытии popup, значение input полей затирается. 
 */
-
-function popup() {
-  const popup = document.querySelector('.popup');
-  const newName = document.querySelector('.profile-edit__name');
-  const newHobby = document.querySelector('.profile-edit__hobby');
-
+function popupStatus() {
   if (popup.classList.contains('popup_is_disabled')) {
     popup.classList.remove('popup_is_disabled');
     popup.classList.add('popup_is_active');
@@ -22,13 +17,7 @@ function popup() {
 /*
   Функция заполнения input полей из содержимого документа.
 */
-
 function openInfo() {
-  const userName = document.querySelector('.profile__user-name');
-  const userHobby = document.querySelector('.profile__user-hobby');
-  const newName = document.querySelector('.profile-edit__name');
-  const newHobby = document.querySelector('.profile-edit__hobby');
-
   newName.value = userName.textContent;
   newHobby.value = userHobby.textContent;
 };
@@ -36,36 +25,32 @@ function openInfo() {
 /*
   Функция сохранения input полей в содержимое документа.
 */
-
 function saveInfo() {
-  const userName = document.querySelector('.profile__user-name');
-  const userHobby = document.querySelector('.profile__user-hobby');
-  const newName = document.querySelector('.profile-edit__name');
-  const newHobby = document.querySelector('.profile-edit__hobby');
-
+  event.preventDefault();
   userName.textContent = newName.value;
   userHobby.textContent = newHobby.value;
 };
 
-/*
-  Создана коллекция элементов btn, где через цикл прохода каждому элементу
-  колекции присвоин обработчик событий по клику.
-*/
+const popup = document.querySelector('.popup');
+const btnEdit = document.querySelector('.btn_type_edit');
+const btnClose = document.querySelector('.btn_type_close');
+const formProfileEdit = document.querySelector('.profile-edit__form');
+const userName = document.querySelector('.profile__user-name');
+const userHobby = document.querySelector('.profile__user-hobby');
+const newName = document.querySelector('.profile-edit__name');
+const newHobby = document.querySelector('.profile-edit__hobby');
 
-const btn = document.querySelectorAll('.btn');
+btnEdit.addEventListener('click', function () {
+  openInfo();
+  popupStatus();
+});
 
-btn.forEach.call(btn, function (element) {
-  element.addEventListener('click', function () {
-    if (this.classList.contains('btn_type_edit')) {
-      openInfo();
-      popup();
-    }
-    if (this.classList.contains('btn_type_close')) {
-      popup();
-    }
-    if (this.classList.contains('btn_type_save')) {
-      saveInfo();
-      popup();
-    }
-  });
+btnClose.addEventListener('click', function () {
+  popupStatus();
+});
+
+formProfileEdit.addEventListener('submit', function (event) {
+  event.preventDefault();
+  saveInfo();
+  popupStatus();
 });
