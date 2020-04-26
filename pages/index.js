1aec4565af05eb65362e596758c6bf94ad0de072
IIIoKoJIaDbI <iiiokojiadbi@yandex.ru>
2020-04-26 23:58:28 +0300

    feat: лайкнуть сердечко. fix: обновить функцию removeParent

--- pages/index.js
+++ pages/index.js
@@ -83,17 +83,17 @@ const openPopup = (evt) => {
     renderFormEdit(formAdd);
   }
   popup.classList.remove('popup_is_disabled');
-  popup.classList.remove('fade_type_out');
+  popup.classList.remove('popup_anim_fade-out');
   popup.classList.add('popup_is_active');
-  popup.classList.add('fade_type_in');
+  popup.classList.add('popup_anim_fade-in');
 };
 
 /*
   Функция закрытия popup
 */
 const closePopup = (evt) => {
-  popup.classList.remove('fade_type_in');
-  popup.classList.add('fade_type_out');
+  popup.classList.remove('popup_anim_fade-in');
+  popup.classList.add('popup_anim_fade-out');
   setTimeout(() => {
     popup.classList.add('popup_is_disabled');
     popup.classList.remove('popup_is_active');
@@ -102,44 +102,25 @@ const closePopup = (evt) => {
 };
 
 /*
-  Функция удаления картинки и формы popup
+  Функция лайка карточки
+*/
+const likeHeart = (evt) => {
+  evt.target.classList.toggle('btn_type_like');
+}
+
+/*
+  Страшная функция удаления картинки, формы popup, и preview картинки.
+  Она страшная, потому что когда в эвенте находится не close кнопка, а форма, работает не так как задумывалась.
+  Тут я ищу класс родителя и подставляю его после шаблонной строкой.
+  Функция универсальная. 
 */
 const removeParent = (evt) => {
-  let classParent = Array.from(evt.target.parentElement.classList).filter(item => item === 'form' || item === 'element' || item === 'preview-image').shift();
-  if (classParent === 'form' || classParent === 'element' || classParent === 'preview-image') {
+  let classParent = Array.from(evt.target.parentElement.classList).find(item => item === 'form' || item === 'element' || item === 'preview-image');
+  if (classParent) { // если необходимые классы не найдены, будет undefined, условие не выполнится
     evt.target.closest(`.${classParent}`).remove();
   } else {
-    const classChildren = Array.from(evt.target.children).find(item => item.classList.contains('btn'))
-    classParent = Array.from(classChildren.parentElement.classList).filter(item => item === 'form');
+    classParent = Array.from(evt.target.classList).find(item => item === 'form');
     evt.target.closest(`.${classParent}`).remove();
-  }
-};
-
-/*
-  Вешаем на отрисованные кнопки (edit и add) открытие popup
-*/
-btn.forEach((item) => {
-  item.addEventListener('click', openPopup);
-});
-
-/*
-  Функция создания карточки
-  oneCard - значение true - 1 карточка, false - нет
-*/
-const renderCards = (oneCard, newName, newSub, newAlt = 'Изображение новой карточки с произвольным изображением') => {
-  const cardTemplate = document.querySelector('#card').content;
-  const cardElement = cardTemplate.cloneNode(true);
-  const cardContainer = document.querySelector('.elements');
-  cardElement.querySelector('.element__title').textContent = newName;
-  cardElement.querySelector('.element__img').src = newSub;
-  cardElement.querySelector('.element__img').alt = newAlt;
-  cardElement.querySelector('.element__img').addEventListener('click', renderPreview);
-  cardElement.querySelector('.element__img').addEventListener('click', openPopup);
-  cardElement.querySelector('.element__btn-trash').addEventListener('click', removeParent);
-  if (!oneCard) {
-    cardContainer.append(cardElement);
-  } else {
-    cardContainer.prepend(cardElement);
   }
 };
 
@@ -162,7 +143,7 @@ const renderFormEdit = (arrForms) => {
 
 /*
   Функция создания preview картинки по клику на ней
-  Блок создается из шаблон
+  Блок создается из шаблона
 */
 const renderPreview = (evt) => {
   const targetParent = evt.target.parentElement;
@@ -177,22 +158,51 @@ const renderPreview = (evt) => {
 }
 
 /*
+  Функция создания карточки
+  oneCard - значение true - 1 карточка, false - нет
+*/
+const renderCards = (oneCard, newName, newSub, newAlt = 'Изображение новой карточки с произвольным изображением') => {
+  const cardTemplate = document.querySelector('#card').content;
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardContainer = document.querySelector('.elements');
+  cardElement.querySelector('.element__title').textContent = newName;
+  cardElement.querySelector('.element__img').src = newSub;
+  cardElement.querySelector('.element__img').alt = newAlt;
+  cardElement.querySelector('.element__img').addEventListener('click', renderPreview);
+  cardElement.querySelector('.element__img').addEventListener('click', openPopup);
+  cardElement.querySelector('.element__btn-like').addEventListener('click', likeHeart);
+  cardElement.querySelector('.element__btn-trash').addEventListener('click', removeParent);
+  if (!oneCard) {
+    cardContainer.append(cardElement);
+  } else {
+    cardContainer.prepend(cardElement);
+  }
+};
+
+/*
   Обработчик формы
   Использована отмена стандартной формы с переданным в функцию событием
 */
 const formSubmitHandler = (evt) => {
   evt.preventDefault();
-
+  const btnSubmit = evt.target.querySelector('.form__btn-submit');
   const newName = document.querySelector('.form__name').value;
   const newSub = document.querySelector('.form__sub').value;
 
-  if (evt.srcElement[3].getAttribute('aria-label') === 'сохранить') {
+  if (btnSubmit.textContent === 'Сохранить') {
     saveInfo(newName, newSub);
   } else {
     renderCards(true, newName, newSub);
   }
   closePopup(evt);
 };
+
+/*
+  Вешаем на отрисованные кнопки (edit и add) открытие popup
+*/
+btn.forEach((item) => {
+  item.addEventListener('click', openPopup);
+});
 
 /*
   Рисуем 6 дефолтных карточек
