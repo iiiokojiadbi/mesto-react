21a460232c6e24943ab89e64f06cb6d4a3d7c614
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-07-30 22:30:02 +0300

    fix: поправить пропсы для вызова методов

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -32,7 +32,7 @@ class Api {
       });
   }
 
-  updateUserInfo({ body }) {
+  updateUserInfo(body) {
     return fetch(`${this._baseUrl}/users/me`, {
       method: 'PATCH',
       headers: this._headers,
@@ -40,7 +40,7 @@ class Api {
     }).then(this._returnResponse);
   }
 
-  updateUserAvatar({ body }) {
+  updateUserAvatar(body) {
     return fetch(`${this._baseUrl}/users/me/avatar`, {
       method: 'PATCH',
       headers: this._headers,
@@ -48,7 +48,7 @@ class Api {
     }).then(this._returnResponse);
   }
 
-  postCard({ body }) {
+  postCard(body) {
     return fetch(`${this._baseUrl}/cards`, {
       method: 'POST',
       headers: this._headers,
