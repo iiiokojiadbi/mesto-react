1de00524bbf433730e4fab503156fbcad86561cd
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-08-01 00:01:42 +0300

    feat: реализовать спиннер при ожидании ответа сервера

--- src/components/ui/ButtonSubmitForm.js
+++ src/components/ui/ButtonSubmitForm.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+
+import Loader from 'react-loader-spinner';
+
+import { StatusRenderContext } from './../../contexts/StatusRenderContext';
 
 const ButtonSubmitForm = ({ text, label }) => {
+  const isRenderer = useContext(StatusRenderContext);
+
   return (
     <button
       type="submit"
       className="btn btn_type_submit form__btn-submit"
       aria-label={label}
     >
-      {text}
+      {isRenderer ? (
+        <Loader
+          type="ThreeDots"
+          color="#ffffff"
+          height={80}
+          width={80}
+          className="btn__spinner"
+        />
+      ) : (
+        text
+      )}
     </button>
   );
 };
