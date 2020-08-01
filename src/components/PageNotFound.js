acfaa8d3befd0c21320641ea23b7b8690427a032
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-08-02 01:10:21 +0300

    feat: реализовать роутинг. Создать компонент PageNotFound

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import notFoundPage from './../images/404.png';
 
 function PageNotFound() {
   return (
     <div className="not-found">
-      <h3 className="not-found__title">
+      <h2 className="not-found__title">
         <span>404</span> - Страница не найдена
-      </h3>
-      <img className="not-found__image" src={Bye} alt="" />
-      <p className="not-found__text">Ой, здесь ничего нет</p>
-      <Link className="button button_type_to-main" to="/">
+      </h2>
+      <img className="not-found__image" src={notFoundPage} alt="" />
+      <p className="not-found__text">Ой, опять Mesto</p>
+      <Link className="btn btn_type_to-main not-found__to-main" to="/">
         Назад
       </Link>
     </div>
